764894bc17ca8a1af0d1bc3b2d08b2908768cb68
DiegoSantosWS <tec.infor321@gmail.com>
2020-02-19 01:46:33 -0300

    channals

--- dia_03/concorrencia.md
+++ dia_03/concorrencia.md
@@ -71,6 +71,27 @@ func sendDataToChannal() {
 
 ```
 
+```go
+func doisTresQuatroVezes(base int, c chan int) {
+	time.Sleep(time.Second)
+	c <- 2 * base // enviando dados para o canal
+
+	time.Sleep(time.Second)
+	c <- 3 * base
+
+	time.Sleep(3 * time.Second)
+	c <- 4 * base
+}
+func main() {
+	c := make(chan int)
+	go doisTresQuatroVezes(2, c)
+
+	a, b := <-c, <-c // recebendo os dados do canal
+	fmt.Println(a, b)
+
+	fmt.Println(<-c)
+}
+```
 # Defer
 
 
--- dia_03/exgoroutine/channal/cha.go
+++ dia_03/exgoroutine/channal/cha.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	sendDataToChannal()
+
+	c := make(chan int)
+	go doisTresQuatroVezes(2, c)
+
+	a, b := <-c, <-c // recebendo os dados do canal
+	fmt.Println(a, b)
+
+	fmt.Println(<-c)
 }
 
 func sendDataToChannal() {
@@ -13,4 +24,15 @@ func sendDataToChannal() {
 
 	ch <- 2
 	fmt.Println(<-ch)
+}
+
+func doisTresQuatroVezes(base int, c chan int) {
+	time.Sleep(time.Second)
+	c <- 2 * base // enviando dados para o canal
+
+	time.Sleep(time.Second)
+	c <- 3 * base
+
+	time.Sleep(3 * time.Second)
+	c <- 4 * base
 }
